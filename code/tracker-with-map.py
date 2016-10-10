898f18fb4973ff6801cc334eb5c63b052abf57f2
Marc Scott <marc@raspberrypi.org>
2016-10-10 08:56:10 +0100

    update

--- code/tracker-with-map.py
+++ code/tracker-with-map.py
@@ -4,6 +4,9 @@
 
 import subprocess
 import crcmod
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
 
 rtl = subprocess.Popen(['rtl_fm', '-M', 'usb', '-f', '434.100M', '-s', '48k'],stdout=subprocess.PIPE)
 sox = subprocess.Popen(['sox', '-t', 'raw', '-r', '48k', '-e', 's', '-b', '16', '-c', '1', '-V1', '-', '-t', 'wav', '-'],stdin=rtl.stdout, stdout=subprocess.PIPE)
@@ -16,6 +19,8 @@ def crc16_ccitt(data):
     crc16 = crcmod.predefined.mkCrcFun('crc-ccitt-false')
     return hex(crc16(data))[2:].upper().zfill(4)
 
+plt.ion()
+
 for line in iter(minimodem.stdout.readline, ''):
     fail += 1
     print('Fail:Success ratio = {0}:{1}'.format(fail,success))
@@ -23,8 +28,13 @@ for line in iter(minimodem.stdout.readline, ''):
         parity = line.decode('utf-8').split(',')[-1].split('*')[1].rstrip()
         data = line.decode('utf-8').replace("$","")[0:-6]
         if crc16_ccitt(str.encode(data)) == parity:
-            print('YEAH GOT IT:',data)
             success += 1
             fail -= 1
+            long, lat = data.split(',')[3], data.split(',')[4]
+            my_map = Basemap(projection='robin', lat_0 = lat, lon_0 = long, resolution = 'l' , area_thresh = 1000.0, llcrnrlon=long-20, llcrnlat=lat-20, urcrnrlon=long+20, urcrnrlat=lat+20)
+            my_map.drawcoastlines()
+            my_map.drawcountries()
+            my_map.fillcontinents(color='coral')
+            plt.draw()
     except:
          pass
